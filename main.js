8d37e711ee50145582e3bd40b3b89a04aec4c1e3
eehvoyage <elizabeth.hart@fora.travel>
2026-04-23 19:20:53 -0500

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll(".accordion").forEach(btn => {
-  btn.addEventListener("click", () => {
-    const panel = btn.nextElementSibling;
-    const open = panel.style.display === "block";
-    panel.style.display = open ? "none" : "block";
-  });
-});
